6c5213b4833591dc00f3f52409a84520027d4352
Saulo Pedro <saulods.pedro@gmail.com>
2017-11-11 16:49:37 -0500

    WARC files can now be created from contextual patterns

--- genWarc/uriExtractor.py
+++ genWarc/uriExtractor.py
@@ -2,11 +2,12 @@ import json
 import sys
 
 json_file=sys.argv[1]
+output_file=sys.argv[2]
 
 with open(json_file) as data_file:
     data = json.load(data_file)
 
-output = open('uri.list', 'w')
+output = open(output_file, 'w')
 
 for dc in data['concept_suggestions']:
     output.write(dc['uri'] + '\n')
