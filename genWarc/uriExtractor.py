5452d10ef8d60f3de55e87bb1c71615ad7abb3f4
Saulo Pedro <saulods.pedro@gmail.com>
2017-11-20 16:08:37 -0500

    changed gen_ctx_patterns to handle articles from keywords search

--- genWarc/uriExtractor.py
+++ genWarc/uriExtractor.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import pprint
 
 json_file=sys.argv[1]
 output_file=sys.argv[2]
@@ -9,8 +10,9 @@ with open(json_file) as data_file:
 
 output = open(output_file, 'w')
 
-for dc in data['concept_suggestions']:
-    output.write(dc['uri'] + '\n')
+for re in data['related_events']:
+    for ar in re['articles']:
+        output.write(ar + '\n')
 
 output.close()
 
